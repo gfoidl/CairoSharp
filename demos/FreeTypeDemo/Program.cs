9bcd56cdd5f0225b38c67ef5f19ca1ffc28ff59d
Günther Foidl <gue@korporal.at>
2025-10-12 18:28:42 +0200

    Simplified the demo program

--- demos/FreeTypeDemo/Program.cs
+++ demos/FreeTypeDemo/Program.cs
@@ -1,82 +1,37 @@
 // (c) gfoidl, all rights reserved
 
-using System.Runtime.InteropServices;
 using Cairo;
 using Cairo.Fonts.FreeType;
 using Cairo.Surfaces.PDF;
 using Cairo.Surfaces.SVG;
 using Cairo.Surfaces.Tee;
-using FreeTypeSharp;
 
 unsafe
 {
-    FT_LibraryRec_* freeTypeLibrary;
-    FT_Error ftStatus = FT.FT_Init_FreeType(&freeTypeLibrary);
+    Console.WriteLine($"FreeType version: {FreeTypeFont.FreeTypeLibVersion()}");
 
-    if (ftStatus != FT_Error.FT_Err_Ok)
-    {
-        Console.WriteLine(ftStatus);
-        Environment.Exit(1);
-    }
+    using FreeTypeFont freeTypeFont = FreeTypeFont.LoadFromFile("SanRemo.ttf");
 
-    PrintFreeTypeVersion(freeTypeLibrary);
+    using SvgSurface svg = new("test.svg", 400, 200);
+    using PdfSurface pdf = new("test.pdf", 400, 200);
+    using TeeSurface tee = new(svg);
+    tee.Add(pdf);
+    using CairoContext cr = new(tee);
 
-    FT_FaceRec_* face;
-    ftStatus = FT.FT_New_Face(freeTypeLibrary, (byte*)Marshal.StringToHGlobalAnsi("SanRemo.ttf"), 0, &face);
+    cr.Rectangle(0, 0, 400, 200);
+    cr.Stroke();
 
-    if (ftStatus != FT_Error.FT_Err_Ok)
-    {
-        Console.WriteLine(ftStatus);
-        Environment.Exit(1);
-    }
+    cr.FontFace = freeTypeFont;
+    cr.SetFontSize(28);
 
-    try
-    {
-        using SvgSurface svg = new("test.svg", 400, 200);
-        using PdfSurface pdf = new("test.pdf", 400, 200);
-        using TeeSurface tee = new(svg);
-        tee.Add(pdf);
-        using CairoContext cr = new(tee);
+    const string Text = "Hello from CairoSharp";
+    cr.MoveTo(10, 100);
+    cr.ShowTextGlyphs(Text);
 
-        cr.Rectangle(0, 0, 400, 200);
-        cr.Stroke();
-
-        using FreeTypeFont freeTypeFont = new(new IntPtr(face), 0);
-        cr.FontFace = freeTypeFont;
-        cr.SetFontSize(28);
-
-        const string Text = "Hello from CairoSharp";
-        cr.MoveTo(10, 100);
-        cr.ShowTextGlyphs(Text);
-
-        tee.WriteToPng("test.png");
-    }
-    finally
-    {
-        ftStatus = FT.FT_Done_Face(face);
-
-        if (ftStatus != FT_Error.FT_Err_Ok)
-        {
-            Console.WriteLine(ftStatus);
-            Environment.Exit(1);
-        }
-
-        // https://freetype.org/freetype2/docs/reference/ft2-library_setup.html#ft_done_freetype
-        // This call frees also all children, so the above call isn't needed.
-        ftStatus = FT.FT_Done_FreeType(freeTypeLibrary);
-
-        if (ftStatus != FT_Error.FT_Err_Ok)
-        {
-            Console.WriteLine(ftStatus);
-            Environment.Exit(1);
-        }
-    }
+    tee.WriteToPng("test.png");
 }
-//-----------------------------------------------------------------------------
-static unsafe void PrintFreeTypeVersion(FT_LibraryRec_* freeTypeLibrary)
-{
-    int major, minor, patch;
-    FT.FT_Library_Version(freeTypeLibrary, &major, &minor, &patch);
 
-    Console.WriteLine($"FreeType version: {major}.{minor}.{patch}");
-}
+// This is not really needed, but here just to showcase
+// BUT: when called it MUST be outside the scopes of the surfaces, because for
+// rendering they need FreeType, so it must not be destroyed before.
+FreeTypeFont.DoneFreeType();
