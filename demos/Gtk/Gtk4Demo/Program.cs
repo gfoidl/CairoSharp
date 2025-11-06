853f0aa2f464d85234b69576b2ce8771de8ca205
Günther Foidl <gue@korporal.at>
2025-11-06 17:22:41 +0100

    Disable client-side decoration on Windows to have native look and feel

--- demos/Gtk/Gtk4Demo/Program.cs
+++ demos/Gtk/Gtk4Demo/Program.cs
@@ -5,6 +5,9 @@ using Gtk4Demo;
 
 if (OperatingSystem.IsWindows())
 {
+    // client-side decorations for a more Windows-like look and feel, cf. https://docs.gtk.org/gtk4/running.html#gtk_csd
+    Environment.SetEnvironmentVariable("GTK_CSD", "0");
+
     // Renders are more beautiful Window w/o any artifacts from the rounded edges on Windows.
     Environment.SetEnvironmentVariable("GSK_RENDERER", "vulkan");
 
